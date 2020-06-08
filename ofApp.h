ccb3f707610986e68fa6553f5c254438af8d5ed9
Junddo <junseomaple@gmail.com>
2020-06-08 23:33:00 +0900

    initial commit

---
+++ ofApp.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxWinMenu.h" // Menu addon
+
+class ofApp : public ofBaseApp {
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key); // Traps escape key if exit disabled
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		bool readFile();
+		void freeMemory();
+		bool DFS();
+		void dfsdraw();
+		int HEIGHT;//미로의 높이
+		int WIDTH;//미로의 너비
+		char** input;//텍스트 파일의 모든 정보를 담는 이차원 배열이다.
+		int** visited;//방문여부를 저장할 포인
+		int maze_col;//미로칸의 열의 인덱스를 가리킨다.
+		int maze_row;//미로칸의 행의 인덱스를 가리킨다.
+		int k;
+		int isOpen; //파일이 열렸는지를 판단하는 변수. 0이면 안열렸고 1이면 열렸다.
+		int isDFS;//DFS함수를 실행시켰는지 판단하는 변수. 0이면 실행안했고 1이면 실행했다.
+		int isBFS;//BFS함수를 실행시켰는지 판단하는 변수. 0이면 실행안했고 1이면 실행했다.
+		// Menu
+		ofxWinMenu * menu; // Menu object
+		void appMenuFunction(string title, bool bChecked); // Menu return function
+
+		// Used by example app
+		ofTrueTypeFont myFont;
+        ofImage myImage;
+		float windowWidth, windowHeight;
+		HWND hWnd; // Application window
+		HWND hWndForeground; // current foreground window
+
+		// Example menu variables
+		bool bShowInfo;
+		bool bFullscreen;
+		bool bTopmost;
+		bool isdfs;
+		// Example functions
+ 		void doFullScreen(bool bFull);
+		void doTopmost(bool bTop);
+
+		//mine
+		
+
+		typedef struct Room {
+			int down;
+			int right;
+			int vertex;
+		}Room;
+
+		Room** maze;
+		int N, M;
+
+		int margin = 10;
+		int w_room = 40;
+		int h_room = 40;
+
+		typedef struct G_queue* G_queuePointer;
+		typedef struct {
+			int vertex;
+			G_queuePointer link;
+		} G_queue;
+		G_queuePointer front, rear;
+};
